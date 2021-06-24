4465fa51dccfb10fd18708d0b495b277cf8465fb
LUIS <luis.debia@gmail.com>
2021-06-24 00:44:17 -0400

    actualizado

--- ventas/ventas/settings.py
+++ ventas/ventas/settings.py
@@ -149,4 +149,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER="djangopython75@gmail.com"
-EMAIL_HOST_PASSWORD = "Qa123098"
+EMAIL_HOST_PASSWORD = "xxxxxxxxx"
